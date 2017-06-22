0c657b38616e2cbfd0095adb82aebe2ed5954463
Ole <ole@vprsm.de>
2017-06-22 21:43:52 +0200

    Fix typo

--- ets/exports_schema13.go
+++ ets/exports_schema13.go
@@ -5,7 +5,7 @@ package ets
 
 import "encoding/xml"
 
-// unmarshalProject11 extracts project information from the current element.
+// unmarshalProject13 extracts project information from the current element.
 func unmarshalProjectInfo13(d *xml.Decoder, start xml.StartElement, proj *ProjectInfo) error {
 	// Schema 11 and 13 are compatible for our purposes.
 	return unmarshalProjectInfo11(d, start, proj)
